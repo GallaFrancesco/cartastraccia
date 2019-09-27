9e87901b851b6aa94072fcc97c4c925ba85c8217
GallaFrancesco <francesco.galla3@gmail.com>
2019-09-27 16:19:30 +0200

    app.d->main.d

--- source/main.d
+++ source/main.d
@@ -1,4 +1,27 @@
-module app;
+/**
+ * Copyright (c) 2019 Francesco Galla` - <me@fragal.eu>
+ *
+ * This file is part of cartastraccia.
+ *
+ * cartastraccia is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * cartastraccia is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with cartastraccia.  If not, see <https://www.gnu.org/licenses/>.
+ * ---
+ *
+ * Main program launcher.
+ *
+*/
+
+module main;
 
 import cartastraccia.config;
 import cartastraccia.asciiart;
